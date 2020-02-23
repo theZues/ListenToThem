b1b37f5681a2fa53c8eedb9d1930731b992b5100
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-02-23 14:36:07 -0600

    Got the quotes to randomly appear including the author

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 
 
-
-
-
-
-
-
 const settings = {
   "async": true,
   "crossDomain": true,
@@ -15,20 +9,34 @@ const settings = {
 
 $.ajax(settings).done(function (response) {
   const data = JSON.parse(response);
-  console.log(data);
+    console.log(data);
 
-  const randQuote = Math.floor(Math.random() * data.length);
-    let randQuote1 = data[randQuote].text;
-    console.log(randQuote1);
-
+  const randNum1 = Math.floor(Math.random() * data.length);
+  const randNum2 = Math.floor(Math.random() * data.length);
 
 
+  const moreQuotes = () => {
 
+    let quote1 = data[randNum1].text;
+    let author1 = data[randNum1].author;
 
+      // console.log(quote1);
+      // console.log(author1);
 
+    let quote2 = data[randNum2].text;
+    let author2 = data[randNum2].author;
+      // console.log(quote2);
+      // console.log(author2);
 
+    $('.quote1').append(quote1);
+    $('.quote2').append(quote2);
+    $('.author1').append(author1);
+    $('.author2').append(author2);
+  }
+moreQuotes();
 
-
-
+  // $('.moreQuotesButton').on('click', () => {
+  //   moreQuotes();
+  // })
 
 });
