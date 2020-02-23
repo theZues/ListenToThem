9f45870c64ed99a7046868170e9d393f29f9c019
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-02-22 23:04:25 -0600

    got a random quote generating

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log($);
+const settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://type.fit/api/quotes",
+  "method": "GET"
+}
+
+$.ajax(settings).done(function (response) {
+  const data = JSON.parse(response);
+  console.log(data);
+
+  const randQuote = Math.floor(Math.random() * data.length);
+    let randQuote1 = data[randQuote].text;
+    console.log(randQuote1);
+
+
+
+
+
+
+
+
+
+
+
+});
