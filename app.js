af82b496d5784491d10bc3f5e7e367ef7d64fd9e
Jesus Vasquez <jesusvasquez@Jesuss-iMac.local>
2020-02-24 14:34:47 -0600

    got my array from api to filter out null value in authors property

--- app.js
+++ app.js
@@ -11,14 +11,22 @@ $.ajax(settings).done(function (response) {
   const data = JSON.parse(response);
     console.log(data);
 
-  const randNum1 = Math.floor(Math.random() * data.length);
-  const randNum2 = Math.floor(Math.random() * data.length);
+  let filteredData = [];
+  for (let i = 0; i < data.length; i++) {
+      if (data[i].author !== null) {
+        filteredData.push(data[i]);
+      }
+   }
+   console.log(filteredData);
+
+  const randNum1 = Math.floor(Math.random() * filteredData.length);
+  const randNum2 = Math.floor(Math.random() * filteredData.length);
 
 
   const moreQuotes = () => {
 
-    let quote1 = data[randNum1].text;
-    let author1 = data[randNum1].author;
+    let quote1 = filteredData[randNum1].text;
+    let author1 = filteredData[randNum1].author;
 
       // console.log(quote1);
       // console.log(author1);
@@ -29,14 +37,60 @@ $.ajax(settings).done(function (response) {
       // console.log(author2);
 
     $('.quote1').append(quote1);
+    $('.author1').append('\"' + author1 +'\"');
     $('.quote2').append(quote2);
-    $('.author1').append(author1);
-    $('.author2').append(author2);
+    $('.author2').append('\"'+ author2 +'\"');
   }
 moreQuotes();
 
-  // $('.moreQuotesButton').on('click', () => {
-  //   moreQuotes();
-  // })
+  $('.moreQuotesButton').on('click', () => {
+      window.location.reload();
+  })
+
 
 });
+
+const $beforeYouGoBtn = $('#openModal');
+const $modal = $('#modal');
+const $closeBtn = $('.close');
+
+const openModal = () => {
+    $modal.css('display', 'block');
+}
+
+const closeModal = () => {
+    $modal.css('display', 'none');
+}
+
+$beforeYouGoBtn.on('click', openModal);
+$closeBtn.on('click', closeModal);
+
+
+
+//================This was me trying to do Google Books API======================
+//Kept getting a 403 forbidden error
+
+
+// $('form').on('submit', (event) => {
+//
+//     event.preventDefault();
+//
+//     const userInput = $('#books').val();  //don't use same style quotes
+//
+//
+//     $.ajax({
+//           url:'http://www.googleapis.com/books/v1/volumes?q=' + userInput +'&key=AIzaSyDTZixDRWuL9DLCI9_JpjtIpgKN41p1ZC8',
+//           type: 'GET'
+//
+//     }).then(
+//       (data) => {
+//           console.log(data);
+//           // $('#title').html(data.Title);
+//           // $('#year').html(data.Year);
+//           // $('#rating').html(data.Rated);
+//       },
+//       (error) => {
+//           console.log('bad request');
+//       }
+//     );
+//   })
